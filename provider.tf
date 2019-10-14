ec04430781d8fcd80ba4cbeeb2ae780001b2f1bf
Tukutozukarin <benle98@live.com>
2019-10-14 12:05:12 +0200

    terra

--- provider.tf
+++ provider.tf
@@ -2,7 +2,11 @@ terraform {
   backend "s3" {
   bucket = "pgr301bucket"
   key    = "usertester/terraform.tfstate"
-  apikey="n8bdJFSIcUpXzfEsACDTZ6wYmKH5x3"
   region = "eu-north-1"
 }
+}
+
+provider "statuscake" {
+version = "1.0.0"
+username = "usertester"
 }
