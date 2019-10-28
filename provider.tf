0240fd9621a4cc2703aa150b0a4f04bc3278d96a
Tukutozukarin <benle98@live.com>
2019-10-28 11:18:53 +0100

    aws

--- provider.tf
+++ provider.tf
@@ -11,6 +11,17 @@ version = "1.0.0"
 username = "usertester"
 }
 
+provider "aws" {
+region     = "us-west-2"
+access_key = "my-access-key"
+secret_key = "my-secret-key"
+}
+
+provider "aws" {
+version = "~> 2.0"
+region  = "us-east-1"
+}
+
 provider "opsgenie" {
     api_key= "OPSGENIE_API_KEY"
     api_url="usertester90.eu.opsgenie.com"
